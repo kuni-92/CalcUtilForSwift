cdf242d4b0461db186444ab3fb4ccdd4a95f929a
kunikuni03 <49485046+kunikuni03@users.noreply.github.com>
2022-02-21 21:19:53 +0900

    Initial Commit

---
+++ CalcUtilForSwift/CalcUtilForSwift.h
@@ -0,0 +1,19 @@
+//
+//  CalcUtilForSwift.h
+//  CalcUtilForSwift
+//  
+//  Created by kunikuni03 on 2022/02/21
+//  
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CalcUtilForSwift.
+FOUNDATION_EXPORT double CalcUtilForSwiftVersionNumber;
+
+//! Project version string for CalcUtilForSwift.
+FOUNDATION_EXPORT const unsigned char CalcUtilForSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CalcUtilForSwift/PublicHeader.h>
+
+
---
+++ StandardCalc.swift
@@ -0,0 +1,16 @@
+//
+//  StandardCalc.swift
+//  CalcUtilForSwift
+//  
+//  Created by kunikuni03 on 2022/02/21
+//  
+//
+
+import Foundation
+
+class StandardCalc {
+    /// Returns the sum of the arguments.
+    func Sum(_ x: Int, _ y: Int) -> Int {
+        return x + y
+    }
+}
